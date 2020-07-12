0d4a17c574e66df828d45eb1d88b50ce72eb881e
Joao Vizu <joaovizu2015@gmail.com>
2020-07-12 19:39:13 -0300

    Arrumando chamada da função scrollBy

--- script/script.js
+++ script/script.js
@@ -1,8 +1,13 @@
 document.querySelector('#items')
   .addEventListener('wheel', event => {
     if(event.deltaY > 0){
-      event.target.scollBY(300, 0)
+      event.target.scrollBy(300, 0)
     }else {
-      event.target.scollBY(-300, 0)
+      event.target.scrollBy(-300, 0)
     }
   })
+
+  document.querySelector('.item')
+    .addEventListener('click', event => {
+      console.log(event.target)
+    })
