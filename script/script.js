a163a1b7e56c96fc7514dc992797d03769728473
Joao Vizu <joaovizu2015@gmail.com>
2020-07-12 19:48:15 -0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,13 +1,9 @@
 document.querySelector('#items')
   .addEventListener('wheel', event => {
     if(event.deltaY > 0){
+      
       event.target.scrollBy(300, 0)
     }else {
       event.target.scrollBy(-300, 0)
     }
   })
-
-  document.querySelector('.item')
-    .addEventListener('click', event => {
-      console.log(event.target)
-    })
